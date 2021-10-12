50e2c4e2795b291556736cd5302739441ef2825c
Sunjuhyeong <67995959+Sunjuhyeong@users.noreply.github.com>
2021-10-12 23:59:22 +0900

    asdf

---
+++ Q1/Q1.py
@@ -0,0 +1,16 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+from IPython import get_ipython
+
+# %%
+# get_ipython().system('pip install -U ipykernel')
+
+
+# %%
+print("hello")
+print("hello2")
+x = 1;
+x = 2;
+print(x)
+
