6c04dd6bdb168d1a736f478f19b0af7cad62d8f7
Sunjuhyeong <67995959+Sunjuhyeong@users.noreply.github.com>
2021-10-13 22:42:20 +0900

    PCA

--- Q1/Q1.py
+++ Q1/Q1.py
@@ -10,6 +10,10 @@ from IPython import get_ipython
 
 import argparse
 import os
+
+from numpy.lib.arraysetops import setdiff1d
+from numpy.linalg.linalg import eig
+from torch._C import dtype
 os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 import copy
@@ -25,7 +29,7 @@ from IPython.display import HTML
 from einops import rearrange, repeat
 import sklearn.cluster
 from sklearn.cluster import AgglomerativeClustering
-from sklearn.datasets import fetch_olivetti_faces
+from sklearn.model_selection import train_test_split 
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from sklearn.metrics.cluster import normalized_mutual_info_score
@@ -88,9 +92,14 @@ def sandbox():
   clustering = AgglomerativeClustering(n_clusters=2, linkage='complete').fit(X)
   clustering.labels_
 
-  v1 = (0.2+0.3+0.4+0.5+0.1) / 3.0
-  v2 = 0.2+0.3+0.1
-  v3 = (0.5+0.1+0.1+0.4)/2.0
+def show_image(images, h, w):
+  plot_n = h * w
+  fig = plt.figure(figsize=(6,6))
+  fig.subplots_adjust(left=0, right=1, bottom=0, top=1, hspace=0.05, wspace=0.05)
+  for i in range(plot_n):
+    ax = fig.add_subplot(h, w, i+1, xticks=[], yticks=[])
+    ax.imshow(images[i], cmap=plt.cm.bone, interpolation='nearest')
+  plt.show()
 
 
 def main():
@@ -98,24 +107,65 @@ def main():
   mat = scipy.io.loadmat('./face.mat')
   H = 46
   W = 56
+  D = H*W 
   n_image = len(mat['l'][0])
+  n_image_test = int( n_image*(0.2))
+  n_image_train = n_image - n_image_test
   n_image_per_person = 10
-  n_people = n_image / n_image_per_person
-  images_data = np.array(mat['X']);
-  images_data = np.transpose(images_data);
-  images = rearrange(images_data, 'N (H W) -> N H W', N=n_image, H=H, W=W);
-  labels = np.array(mat['l']);
-  labels = np.transpose(labels);
-  
-  # Plot Image  
-  fig = plt.figure(figsize=(100,100))
-  fig.subplots_adjust(left=0, right=1, bottom=0, top=1, hspace=0.05, wspace=0.05)
-  for i in range(n_image):
-      ax = fig.add_subplot(n_people, n_image_per_person, i+1, xticks=[], yticks=[])
-      ax.imshow(images[i], cmap=plt.cm.bone, interpolation='nearest')
+  n_people = int(n_image / n_image_per_person)
+  images_data = np.array(mat['X'])
+  images_data = np.transpose(images_data)
+  labels = np.array(mat['l'])
+  labels = np.transpose(labels)
+  x_train, x_test, y_train, y_test = train_test_split(images_data, labels,
+            test_size=0.2, shuffle=True, stratify=labels, random_state = 34)
 
+  # Training
+  x_mean = np.mean(x_train, axis=0) # 1, D
+  x = x_train - x_mean # N, D
+  x = x.T
+  cov_x = np.cov(x)
+  eig_values, eig_vectors = np.linalg.eig(cov_x,)
+  eig_values = np.real(eig_values)
+  eig_vectors = np.real(eig_vectors)
+  
+  # Zero eigen Value Index
+  zero_eig_idx = np.where(eig_values < 10**-6)
+
+  # Plot Mean Face  
+  images = rearrange(x_mean, '(H W) -> H W', H=H, W=W)
+  images = repeat(images, 'H W -> 1 H W')
+  # show_image(images, 1, 1)
+  
+  # Plot EigenFace
+  temp_eig_vectors = rearrange(eig_vectors, '(H W) N  -> N H W', N=H*W, H=H, W=W)
+  fig = plt.figure(figsize=(8,8)) 
+  fig.subplots_adjust(left=0, right=1, bottom=0, top=1, hspace=0.05, wspace=0.05)
+  for i in range(10): 
+    ax = fig.add_subplot(2, 5, i+1, xticks=[], yticks=[]) 
+    ax.imshow(np.reshape(temp_eig_vectors[i], (46,56)), cmap=plt.cm.bone, interpolation='nearest') 
   plt.show()
 
+
+
+  # x_test: 104, 2576
+  # Reconstruction
+  M = 80 # used_eigenvectors
+  indices = np.arange(M)
+  principal_eigen_vectors = np.take(eig_vectors, indices, axis=-1) # D, M 
+  proj_x = (x_test - x_mean) @ principal_eigen_vectors # N_test, M
+  proj_x_inverse = proj_x @ (principal_eigen_vectors.T) # N_test, D
+  result = proj_x_inverse + x_mean # N_test, D
+  
+  #  Plot Test Image  
+  plot_n = 10
+  images = np.take(result, np.arange(plot_n), axis=0)
+  images = rearrange(images, 'N (H W) -> N H W', N=plot_n, H=H, W=W)
+  images_gt = np.take(x_test, np.arange(plot_n), axis=0)
+  images_gt = rearrange(images_gt, 'N (H W) -> N H W', N=plot_n, H=H, W=W)
+  
+  show_image(np.concatenate([images_gt, images], axis=0), 2, 10)
+  # show_image(images, 5, 2)
 
 if __name__ == "__main__":
   	main()
